efb86d0fd209ecfcf81dd98f00038a81076a5c34
Romilson Nunes <souzainf3@yahoo.com.br>
2018-01-28 18:46:04 -0200

    Initial Commit

---
+++ MaskTextFieldUITests/MaskTextFieldUITests.swift
@@ -0,0 +1,36 @@
+//
+//  MaskTextFieldUITests.swift
+//  MaskTextFieldUITests
+//
+//  Created by Romilson Nunes on 28/01/18.
+//  Copyright © 2018 Romilson Nunes. All rights reserved.
+//
+
+import XCTest
+
+class MaskTextFieldUITests: XCTestCase {
+        
+    override func setUp() {
+        super.setUp()
+        
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+        
+        // In UI tests it is usually best to stop immediately when a failure occurs.
+        continueAfterFailure = false
+        // UI tests must launch the application that they test. Doing this in setup will make sure it happens for each test method.
+        XCUIApplication().launch()
+
+        // In UI tests it’s important to set the initial state - such as interface orientation - required for your tests before they run. The setUp method is a good place to do this.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // Use recording to get started writing UI tests.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+}
